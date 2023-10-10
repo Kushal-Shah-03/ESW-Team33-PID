a0832a0bd535605a9244521c923cfa2c6f600c88
Umang Patel <umangpatel339@pop-os.localdomain>
2023-10-10 15:36:40 +0530

    feat: node api added

---
+++ node-api/app.js
@@ -0,0 +1,85 @@
+// Import required modules
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var rpm = 0;
+var kp = 0;
+var ki = 0;
+var kd = 0;
+var count = 0;
+
+// Create an instance of express
+const app = express();
+
+// Set up middleware
+app.use(bodyParser.json());
+
+// Import routes
+
+// Catch all other routes and return the index file
+app.get('/', (req, res) => {
+    console.log(__dirname);
+    // res.send("Hello World");
+    const path = require('path');
+    const full_path = path.join(__dirname, '../public/index.html');
+    res.sendFile(full_path);
+}
+);
+
+app.post('/api/updateRPM', (req, res) => {
+    console.log(req.body);
+    rpm = req.body.rpm;
+    console.log("RPM:", rpm);
+    kp = req.body.kp;
+    ki = req.body.ki;
+    kd = req.body.kd;
+    console.log("Kp:", kp);
+    console.log("Ki:", ki);
+    console.log("Kd:", kd);
+    count++;
+    res.send("OK");
+});
+
+app.get('/api/getRPM', (req, res) => {
+    if (count > 0) {
+        res.send(rpm);
+    }
+    else {
+        res.send("No data");
+    }
+}
+);
+
+app.get('/api/getkp', (req, res) => {
+    if (count > 0) {
+        res.send(kp);
+    }
+    else {
+        res.send("No data");
+    }
+});
+
+app.get('/api/getki', (req, res) => {
+    if (count > 0) {
+        res.send(ki);
+    }
+    else {
+        res.send("No data");
+    }
+}
+);
+
+app.get('/api/getkd', (req, res) => {
+    if (count > 0) {
+        res.send(kd);
+    }
+    else {
+        res.send("No data");
+    }
+}
+);
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
