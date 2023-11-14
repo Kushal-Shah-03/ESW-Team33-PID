806c97f8d2fd52eb50ee0ffbe048cbed91b04b93
Umang Patel <umang.patel@students.iiit.ac.in>
2023-11-14 10:03:38 +0530

    fix : api

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-var rpm = 0;
-var kp = 0;
-var ki = 0;
-var kd = 0;
+var rpm = 80;
+var kp = 5;
+var ki = 0.001;
+var kd = 0.25;
 var count = 0;
 var currrpm = 0;
-var count1=0;
+var count1 = 0;
 // Create an instance of express
 const app = express();
 
@@ -37,10 +37,22 @@ app.get('/graph', (req, res) => {
 app.post('/api/updateRPM', (req, res) => {
     console.log(req.body);
     rpm = req.body.rpm;
-    console.log("RPM:", rpm);
     kp = req.body.kp;
     ki = req.body.ki;
     kd = req.body.kd;
+    if (req.body.rpm == '') {
+        rpm = 80;
+    }
+    if (req.body.kp == '') {
+        kp = 5;
+    }
+    if (req.body.ki == '') {
+        ki = 0.001;
+    }
+    if (req.body.kd == '') {
+        kd = 0.25;
+    }
+    console.log("RPM:", rpm);
     console.log("Kp:", kp);
     console.log("Ki:", ki);
     console.log("Kd:", kd);
@@ -48,44 +60,39 @@ app.post('/api/updateRPM', (req, res) => {
     res.send("OK");
 });
 
-app.get('/api/getRPM', (req, res) => {
-    if (count > 0) {
-        res.send(rpm);
-    }
-    else {
-        res.send("No data");
-    }
+app.get('/api/getdata', (req, res) => {
+    res.send({ RPM: rpm, Kp: kp, Ki: ki, Kd: kd });
 }
 );
 
-app.get('/api/getkp', (req, res) => {
-    if (count > 0) {
-        res.send(kp);
-    }
-    else {
-        res.send("No data");
-    }
-});
+// app.get('/api/getkp', (req, res) => {
+//     if (count > 0) {
+//         res.send(kp);
+//     }
+//     else {
+//         res.send("No data");
+//     }
+// });
 
-app.get('/api/getki', (req, res) => {
-    if (count > 0) {
-        res.send(ki);
-    }
-    else {
-        res.send("No data");
-    }
-}
-);
+// app.get('/api/getki', (req, res) => {
+//     if (count > 0) {
+//         res.send(ki);
+//     }
+//     else {
+//         res.send("No data");
+//     }
+// }
+// );
 
-app.get('/api/getkd', (req, res) => {
-    if (count > 0) {
-        res.send(kd);
-    }
-    else {
-        res.send("No data");
-    }
-}
-);
+// app.get('/api/getkd', (req, res) => {
+//     if (count > 0) {
+//         res.send(kd);
+//     }
+//     else {
+//         res.send("No data");
+//     }
+// }
+// );
 
 app.post('/api/updatecurrRPM', (req, res) => {
     console.log(req.body);
@@ -98,15 +105,13 @@ app.post('/api/updatecurrRPM', (req, res) => {
 
 
 app.get('/api/getCurrRPM', (req, res) => {
-    if(count1>0)
-    {
+    if (count1 > 0) {
         const data = {
             RPM: currrpm
         };
         res.send(data);
     }
-    else
-    {
+    else {
         res.send("0");
     }
 }
